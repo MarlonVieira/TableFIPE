f34832c0d8489fa7580bcaf6cb3918decfafa51b
Marlon_Vieira <marlon.vieira@viasoft.com.br>
2025-05-26 17:25:40 -0300

    Commit new adjusts 05/26/2025

--- src/main/java/br/com/alura/TableFIPE/TableFipeApplication.java
+++ src/main/java/br/com/alura/TableFIPE/TableFipeApplication.java
@@ -1,16 +1,18 @@
 package br.com.alura.TableFIPE;
 
 import br.com.alura.TableFIPE.main.MainTableFIPE;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TableFipeApplication {
+public class TableFipeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TableFipeApplication.class, args);
 	}
 
+	@Override
 	public void run(String... args) throws Exception {
 		MainTableFIPE mainTableFIPE = new MainTableFIPE();
 		mainTableFIPE.ShowMenu();
--- src/main/java/br/com/alura/TableFIPE/main/MainTableFIPE.java
+++ src/main/java/br/com/alura/TableFIPE/main/MainTableFIPE.java
@@ -1,7 +1,17 @@
 package br.com.alura.TableFIPE.main;
 
+import java.util.Scanner;
+
 public class MainTableFIPE {
     public void ShowMenu() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("*** OPTIONS ***");
+        System.out.println("1 - Car" +
+                           "\n2 - Motorcycle" +
+                           "\n3 - Truck"+
+                           "\n***************");
 
+        System.out.printf("Enter an option to consult the values: ");
+        scan.nextLine();
     }
 }
